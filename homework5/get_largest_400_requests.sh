5f89a9fd03248f247e7dfed9f84c39a572a27f76
AngrySigma <illariov1809@gmail.com>
2022-11-13 20:29:08 +0300

    Add empty lines in bash scripts

--- homework5/get_largest_400_requests.sh
+++ homework5/get_largest_400_requests.sh
@@ -1,3 +1,2 @@
 #!/usr/bin/bash
-#sort -r -k 10 access.log | head -5
 cat access.log | grep '[1-9\.]* - - \[.*\] ".*" 4' | sort -r -k 10 | head -5 | awk -F'[ "]' '{print "Adress:" $8, $9, "\nStatus code:" $11,  "\nSize:" $12 , "\nIP:" $1 "\n"}'
