4a7870e558ecd6a5e2efc61614c9938fec888941
AngrySigma <illariov1809@gmail.com>
2022-11-13 20:36:13 +0300

    Fix variable naming

--- homework5/get_request_methods.sh
+++ homework5/get_request_methods.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
-for query in GET HEAD POST PUT DELETE CONNECT OPTIONS TRACE PATCH
+for method in GET HEAD POST PUT DELETE CONNECT OPTIONS TRACE PATCH
 do
-echo $query requests:
-grep "\<$query" access.log | wc -l
+echo $method requests:
+grep "\<$method" access.log | wc -l
 done
